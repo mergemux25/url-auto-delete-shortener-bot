27969b894c209d98e55ab4297c131e92dabdf578
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-02-11 13:09:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -393,7 +393,7 @@ async def get_shortlink(link):
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json()
                 if data["status"] == "success":
-                    return data['shortenedUrl'].replace("https","http")
+                    return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
                    # return f'{link}'
